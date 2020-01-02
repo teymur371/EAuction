b6a7f93f03867c8c12bb3e7d7f6b6be05e20af10
teymur371 <51134276+teymur371@users.noreply.github.com>
2020-01-02 20:54:30 +0200

    Update Item.cs

--- EAuction/Models/Item.cs
+++ EAuction/Models/Item.cs
@@ -18,7 +18,50 @@ namespace EAuction.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public IList<Attachment> Attachments { get; set; }
+        public int UserId { get; set; } // foreign key
+    }
+    
+    public class ItemAuction
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int ItemId {get; set; } //foreign key
+        public Item CurrentItem { get; set; }
         public double StartPrice { get; set; }
         public Currency Currency { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime Deadline { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool IsActive { get { return DateTime.Now < this.Deadline; } }
     }
+    
+    public class ItemAuctionBid
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int AuctionId {get; set; } //foreign key
+        public ItemAuction CurrentAuction { get; set; }
+        public double Price { get; set; }
+        public Currency Currency { get { return this.ItemAuction.Currency; } } 
+        public DateTime MadeDate { get; set; }
+        public string UserId { get; set; } // bids should be anonymous.
+    }
+    
+    public class Attachment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ItemId { get; set; } // foreign key
+    }
+    
+    public class User
+    {
+        // basic info
+    }
+        
 }
